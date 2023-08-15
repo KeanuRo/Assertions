334078da5010cf6f8ec6d244e7e5975ba18bb755
Киану Сорокопуд <keanuben1@gmail.com>
2023-08-15 18:54:46 +0300

    Fix code style

--- src/Assertions.php
+++ src/Assertions.php
@@ -2,36 +2,37 @@
 
 namespace Assert;
 
-abstract class Assertions {
-    public static function string($variable){
-        if (false === is_string($variable)){
-            throw new AssertException('Variable "' . $variable . '" is not a string.');
-        }
-        return __CLASS__;
+class Assertions
+{
+    public static function string(string $variable)
+    {
+        return  __CLASS__;
     }
-    public static function stringNotEmpty($variable){
-        if (false === is_string($variable)){
-            throw new AssertException('Variable "' . $variable . '" is not a string.');
-        }
-        if (strlen($variable) === 0){
+
+    public static function stringNotEmpty(string $variable)
+    {
+        if (strlen($variable) === 0) {
             throw new AssertException('String is empty.');
         }
         return __CLASS__;
     }
-    public static function integer($variable){
-        if (false === is_integer($variable)){
-            throw new AssertException('Variable "' . $variable . '" is not an integer.');
-        }
+
+    public static function integer(int $variable)
+    {
         return __CLASS__;
     }
-    public static function null($variable){
-        if (false === is_null($variable)){
+
+    public static function null(mixed $variable)
+    {
+        if (isset($variable)) {
             throw new AssertException('Variable "' . $variable . '" is not null.');
         }
         return __CLASS__;
     }
-    public static function notNull($variable){
-        if (is_null($variable)){
+
+    public static function notNull(mixed $variable)
+    {
+        if (is_null($variable)) {
             throw new AssertException('Variable is null.');
         }
         return __CLASS__;
